d26f17268b7423d1c51b41660e5c2689f7f24708
kyuho <sgh055@naver.com>
2021-04-29 17:20:35 +0900

    first commit    - room test

---
+++ app/src/main/java/kr/co/kkyu/modernandroid/AppDatabase.java
@@ -0,0 +1,9 @@
+package kr.co.kkyu.modernandroid;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Todo.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract TodoDao todoDao();
+}
